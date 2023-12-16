6a07c34ca229a87c3f5cc30b8d26fbd0009a9547
TalhaAnik56 <aniktalha@gmail.com>
2023-12-16 20:12:05 +0600

    created genre,writer,book,booklist models

--- warehouse/models.py
+++ warehouse/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Genre(models.Model):
+    title=models.CharField(max_length=20)
+    description=models.CharField(max_length=1000)
+    featured_book=models.ForeignKey('Book',on_delete=models.SET_NULL,null=True,related_name='+')
+
+class Writer(models.Model):
+    name=models.CharField(max_length=35)
+    about=models.CharField(max_length=1000)
+
+class Book(models.Model):
+    title=models.CharField(max_length=255)
+    publication=models.CharField(max_length=25)
+    writer=models.ForeignKey(Writer,on_delete=models.PROTECT)
+    genre=models.ForeignKey(Genre,on_delete=models.PROTECT)
+
+class BookList(models.Model):
+    description=models.TextField()
+    unitPrice=models.DecimalField(max_digits=6,decimal_places=2)
+    discountedPrice=models.DecimalField(max_digits=6,decimal_places=2)
+    stock=models.PositiveIntegerField()
+    seller=models.ForeignKey('Seller',on_delete=models.PROTECT)
+    book=models.ForeignKey(Book,on_delete=models.PROTECT)
+
