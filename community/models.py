fa3f159a36f3d3f58bef1a7fabe9092a8ceb7e65
TalhaAnik56 <aniktalha@gmail.com>
2023-12-16 20:44:56 +0600

    created customer,seller and feedback classes

--- community/models.py
+++ community/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+
+class Customer(models.Model):
+    name=models.CharField(max_length=10)
+    address=models.CharField(max_length=50)
+    phoneNo=models.CharField(max_length=12)
+    birthDate=models.DateField(null=True)
+    joined_at=models.DateTimeField(auto_now_add=True)
+
+
+class Seller(models.Model):
+    brandName=models.CharField(max_length=20)
+    address=models.CharField(max_length=50)
+    nid=models.CharField(max_length=30)
+    birthDate=models.DateField(null=True)
+    joined_at=models.DateTimeField(auto_now_add=True)
+
+
+
+
--- warehouse/models.py
+++ warehouse/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+from community.models import Seller,Customer
 
 # Create your models here.
 
@@ -11,18 +13,28 @@ class Genre(models.Model):
 class Writer(models.Model):
     name=models.CharField(max_length=35)
     about=models.CharField(max_length=1000)
+    created_at=models.DateTimeField(auto_now_add=True)
 
 class Book(models.Model):
     title=models.CharField(max_length=255)
     publication=models.CharField(max_length=25)
     writer=models.ForeignKey(Writer,on_delete=models.PROTECT)
     genre=models.ForeignKey(Genre,on_delete=models.PROTECT)
+    created_at=models.DateTimeField(auto_now_add=True)
 
 class BookList(models.Model):
     description=models.TextField()
     unitPrice=models.DecimalField(max_digits=6,decimal_places=2)
     discountedPrice=models.DecimalField(max_digits=6,decimal_places=2)
     stock=models.PositiveIntegerField()
-    seller=models.ForeignKey('Seller',on_delete=models.PROTECT)
+    seller=models.ForeignKey(Seller,on_delete=models.PROTECT)
     book=models.ForeignKey(Book,on_delete=models.PROTECT)
+    created_at=models.DateTimeField(auto_now_add=True)
 
+class Feedback(models.Model):
+    rating=models.PositiveSmallIntegerField(validators=[
+            MaxValueValidator(10, "Rating cannot exceed 10")
+        ],null=True)
+    comment=models.CharField(max_length=1000)
+    book=models.ForeignKey(Book,on_delete=models.CASCADE)
+    customer=models.ForeignKey(Customer,on_delete=models.CASCADE)
