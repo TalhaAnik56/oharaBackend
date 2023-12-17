f46ac536cec7e33c70739e4492d4a9491f297fd0
TalhaAnik56 <aniktalha@gmail.com>
2023-12-17 20:33:13 +0600

    created the tables using migration

--- community/models.py
+++ community/models.py
@@ -12,6 +12,7 @@ class Customer(models.Model):
 class Seller(models.Model):
     brandName=models.CharField(max_length=20)
     address=models.CharField(max_length=50)
+    phoneNo=models.CharField(max_length=12)
     nid=models.CharField(max_length=30)
     birthDate=models.DateField(null=True)
     joined_at=models.DateTimeField(auto_now_add=True)
