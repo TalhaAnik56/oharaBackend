f37645a7f4c60702e18086ab8d1e58044a6baa04
TalhaAnik56 <aniktalha@gmail.com>
2024-01-31 15:39:51 +0600

    implemented pagination in the viewsets

--- warehouse/views.py
+++ warehouse/views.py
@@ -4,6 +4,8 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
+from widespread.paginations import CustomPagination
+
 from .models import Book, BookItem, Feedback, Genre, Writer
 from .serializers import (
     BookItemSerializer,
@@ -53,15 +55,24 @@ class WriterViewSet(ModelViewSet):
 
 
 class BookViewSet(ModelViewSet):
-    queryset = (
-        Book.objects.all()
-        .select_related("writer")
-        .select_related("genre")
-        .annotate(book_item_count=Count("bookitem", distinct=True))
-        .annotate(feedback_count=Count("feedback", distinct=True))
-        .order_by("title")
-    )
+    def get_queryset(self):
+        page_size = self.request.query_params.get("page_size")
+
+        if page_size is not None:
+            CustomPagination.page_size = page_size
+            self.pagination_class = CustomPagination
+
+        queryset = (
+            Book.objects.all()
+            .select_related("writer")
+            .select_related("genre")
+            .annotate(book_item_count=Count("bookitem", distinct=True))
+            .annotate(feedback_count=Count("feedback", distinct=True))
+            .order_by("title")
+        )
+
     serializer_class = BookSerializer
+    pagination_class = CustomPagination
 
     def destroy(self, request, *args, **kwargs):
         book = get_object_or_404(Book, pk=kwargs["pk"])
@@ -78,6 +89,12 @@ class BookViewSet(ModelViewSet):
 
 class BookItemViewSet(ModelViewSet):
     def get_queryset(self):
+        page_size = self.request.query_params.get("page_size")
+
+        if page_size is not None:
+            CustomPagination.page_size = page_size
+            self.pagination_class = CustomPagination
+
         queryset = (
             BookItem.objects.filter(book_id=self.kwargs["book_pk"])
             .select_related("book__writer")
@@ -88,6 +105,7 @@ class BookItemViewSet(ModelViewSet):
         return queryset
 
     serializer_class = BookItemSerializer
+    pagination_class = CustomPagination
 
     def get_serializer_context(self):
         return {"book_id": self.kwargs["book_pk"]}
@@ -95,6 +113,12 @@ class BookItemViewSet(ModelViewSet):
 
 class FeedbackViewSet(ModelViewSet):
     def get_queryset(self):
+        page_size = self.request.query_params.get("page_size")
+
+        if page_size is not None:
+            CustomPagination.page_size = page_size
+            self.pagination_class = CustomPagination
+
         queryset = (
             Feedback.objects.filter(book_id=self.kwargs["book_pk"])
             .select_related("book")
@@ -104,6 +128,7 @@ class FeedbackViewSet(ModelViewSet):
         return queryset
 
     serializer_class = FeedbackSerializer
+    pagination_class = CustomPagination
 
     def get_serializer_context(self):
         return {"book_id": self.kwargs["book_pk"]}
